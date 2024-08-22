d966ccbf64da937845e89aaa429b74cd5d40da7e
wyvernbw <iamwyvern511@gmail.com>
2024-08-23 00:53:14 +0300

    add expr or_else and expect functions

--- maybe.gd
+++ maybe.gd
@@ -1,6 +1,6 @@
 # 🤔 what even is null?
 class_name Maybe
-extends Node
+extends RefCounted
 
 var value
 var has_value: bool = false
@@ -11,6 +11,10 @@ func _init(new_value = null):
 
 func unwrap():
 	assert(self.has_value, "Tried to unwrap a null value!")
+	return self.value
+
+func expect(message: String):
+	assert(self.has_value, message)
 	return self.value
 
 func is_some():
@@ -31,6 +35,20 @@ func unwrap_or_else(fn):
 	else:
 		return fn.call()
 
+func expr():
+	if self.has_value:
+		return { "Some": self.value }
+	else:
+		return "None"
+
+func or_else(other: Maybe) -> Maybe:
+	if self.has_value:
+		return self
+	elif other.has_value:
+		return other
+	else:
+		return self
+	
 static func Some(new_value):
 	return Maybe.new(new_value)
 
